cfdcc9794367123b5ebf76e4a38d9023164461c7
gerardalessandrov <gerardvigorodrigues@gmail.com>
2024-05-18 12:06:31 -0500

    changes

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -7,13 +7,13 @@ public class Program
         while (true)
         {
             Console.Clear();
-            Console.WriteLine("Choose an activity:");
+            Console.WriteLine("Menu Options:");
             Console.WriteLine("1. Start reflecting Activity");
             Console.WriteLine("2. Start breathing Activity");
             Console.WriteLine("3. Start listing Activity");
             Console.WriteLine("4. Start walking Activity");
             Console.WriteLine("5. Exit");
-
+            Console.WriteLine("Select a choice from the menu:");
             string choice = Console.ReadLine();
 
             if (choice == "5")
