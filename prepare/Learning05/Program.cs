47a5dce27e2a27d1a32773515061bf37cd2d3bbf
gerardalessandrov <gerardvigorodrigues@gmail.com>
2024-05-25 13:23:51 -0500

    shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("orange", 22.3));
+        shapes.Add(new Circle("orange", 10.2));
+        shapes.Add(new Rectangle("blue", 22.3, 10.5));
+        foreach (Shape shape in shapes)
+        {
+            double area = shape.GetArea();
+            string color = shape.GetColor();
+
+            Console.WriteLine($"The color is {color} ");
+            Console.WriteLine($"The area is {area} ");
+        }
     }
 }
